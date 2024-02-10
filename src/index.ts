4a031e4be3d1b4950a7b41eaccc5246857d062ac
Trevor Williams <tmw7991@gmail.com>
2024-02-10 10:06:55 -0700

    feat: image reveal animations

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
-import { greetUser } from '$utils/greet';
+import { initAnimations } from './animations';
 
 window.Webflow ||= [];
 window.Webflow.push(() => {
-  const name = 'John Doe';
-  greetUser(name);
+  main();
 });
+
+function main() {
+  console.log('Webflow loaded');
+  initAnimations();
+}
